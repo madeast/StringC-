24adb8a732ee0ea95d220643a3534a142232d16e
Easton Madsen <eeagle25@comcast.net>
2016-01-26 09:09:30 -0700

    Added new methods

--- src/Controller/StringController.cpp
+++ src/Controller/StringController.cpp
@@ -12,7 +12,7 @@ using namespace std;
 
 StringController :: StringController()
 {
-	this->count = "HI ";
+	this->count = "HI";
 }
 
 void StringController :: setString(string count)
@@ -32,8 +32,17 @@ void StringController :: start()
 	cout << "Type in a new String please." << endl;
 	string tempString;
 	cin >> tempString;
-	cout << tempString.length << endl;
+	cout << tempString.substr(0) << endl;
 	setString(tempString);
 	cout << getString() << " is the updated String." << endl;
+
+	if(!count.compare(tempString))
+	{
+		cout << "Your Strings match." << endl;
+	}
+	else
+	{
+		cout << "Your Strings don't match." << endl;
+	}
 }
 
